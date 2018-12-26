0cde4976baf281eba310ed37bb1fc91f5771ef2a
BIT <BIT@DESKTOP-68STVVN>
2018-12-27 08:52:44 +0900

    5번 문제 풀어서 commit

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -3,5 +3,26 @@ package practice01;
 public class Prob5 {
 
 	public static void main(String[] args) {
+
+		int num1 = 1; // 10의자리 수
+		int num2 = 1; // 1의 자리 수
+
+		for (int i = 1; i <= 99; i++) {
+			
+			if(i>10) { //10의자리 분리 위해서..
+				num1 = i / 10;
+			}
+			
+			num2 = i % 10;
+			
+			// 10의 배수이지만 3의 배수가 아닐 경우..
+			if(i%10 == 0 && i%3!=0) continue;
+			
+			if(num1 % 3 == 0 && num2 % 3 == 0) { // 두 수 모두 3의 배수일 경우
+				System.out.println(i + " 짝짝");
+			} else if (num1 % 3 == 0 || num2 % 3 == 0) { // 두 수 중에서 하나라도 3의 배수일 경우
+				System.out.println(i + " 짝");
+			} 
+		}
 	}
 }
